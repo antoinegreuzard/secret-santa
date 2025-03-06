233c5275f573659e87a48ccac44c0e1ec11aa2c7
Antoine Greuzard <antoin.greuzard@gmail.com>
2025-03-06 14:34:19 +0100

    fix: naming route participants.index

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ParticipantController::class, 'index']);
-Route::get('/participants', [ParticipantController::class, 'index']);
+Route::get('/participants', [ParticipantController::class, 'index'])->name('participants.index');
 Route::post('/participants', [ParticipantController::class, 'store']);
 Route::put('/participants/{participant}', [ParticipantController::class, 'update']);
 Route::delete('/participants/{participant}', [ParticipantController::class, 'destroy']);

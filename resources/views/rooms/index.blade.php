8bf3ec9bff1b75109e1b78ed215a3cf6419bfbaf
Antoine Greuzard <antoin.greuzard@gmail.com>
2025-03-07 12:04:47 +0100

    fix: rollback design

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -24,28 +24,22 @@
 
     <form action="/rooms" method="POST" class="mb-6">
         @csrf
-        <div class="flex flex-col space-y-2">
-            <div>
-                <input type="text" name="name" placeholder="Nom de la Room" required
-                       class="p-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500 @error('name') border-red-500 @enderror">
-                @error('name')
-                <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+        <input type="text" name="name" placeholder="Nom de la Room" required
+               class="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 @error('name') border-red-500 @enderror">
+        @error('name')
+        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+        @enderror
 
-            <div>
-                <input type="password" name="password" placeholder="Mot de passe" required
-                       class="p-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500 @error('password') border-red-500 @enderror">
-                @error('password')
-                <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+        <input type="password" name="password" placeholder="Mot de passe" required
+               class="p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 @error('password') border-red-500 @enderror">
+        @error('password')
+        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+        @enderror
 
-            <button type="submit"
-                    class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 cursor-pointer">
-                Créer
-            </button>
-        </div>
+        <button type="submit"
+                class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 cursor-pointer">
+            Créer
+        </button>
     </form>
 
     <ul class="bg-gray-50 p-4 rounded-lg shadow">

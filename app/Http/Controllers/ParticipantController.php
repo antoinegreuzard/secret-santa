cd70da816f21560790f9fba07ab6fb8c26fab18a
Antoine Greuzard <antoin.greuzard@gmail.com>
2025-03-07 12:15:22 +0100

    fix: use session room_id

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -20,17 +20,15 @@ class ParticipantController
 
     public function store(Request $request)
     {
+        $request->merge(['room_id' => session('room_id')]);
+
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:participants,email',
             'room_id' => 'required|exists:rooms,id'
         ]);
 
-        Participant::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'room_id' => $validated['room_id'],
-        ]);
+        Participant::create($validated);
 
         return back()->with('success', 'Participant ajouté avec succès !');
     }
--- resources/views/participants/form.blade.php
+++ resources/views/participants/form.blade.php
@@ -1,12 +1,13 @@
 <form action="/participants" method="POST" class="mb-4 mt-4">
     @csrf
+    <input type="hidden" name="room_id" value="{{ session('room_id') }}">
+
     <div class="flex space-x-2">
         <input type="text" name="name" placeholder="Nom" required
                class="w-1/2 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 @error('name') border-red-500 @enderror">
         @error('name')
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
         @enderror
-
 
         <input type="email" name="email" placeholder="Email" required
                class="w-1/2 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 @error('email') border-red-500 @enderror">
@@ -15,7 +16,7 @@
         @enderror
 
         <button type="submit"
-                class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 cursor-pointer ">
+                class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 cursor-pointer">
             Ajouter
         </button>
     </div>
